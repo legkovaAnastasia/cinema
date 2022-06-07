0c173d1e8c7d4644390bd574668f26e01e1bf283
anastasia <legkova.anastasia127@gmail.com>
2022-06-07 17:58:53 +0300

    make_calendar

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -6,42 +6,19 @@ const Calendar = () => {
 
     const [sessions, setSession] = useState([]);
 
-    let films = ['Зеленая миля', 'Список Шиндлера', 'Побег из Шоушенка', 'Властелин колец: Возвращение короля',
-        'Форрест Гамп', 'Властелин колец: Две крепости', 'Властелин колец: Братство Кольца'];
-    let n = 10;
 
     const sessionRender = () => {
 
-        // do {
-        //     const newSession = {
-        //         time: n += 2,
-        //         timeId: Math.random().toString().substring(2, 4),
-        //         filmName: films[0]
-        //     }
-        //     films.shift();
-        //     setSession([sessions, newSession]);
-        // }
-        // while (n >= 10 && n < 20);
-        // console.log(n);
+        
+        let n = 8;
+        do{
+            n += 2;
+sessions.push(n)
+setSession();
+        } while (n >= 8 && n <= 18)
+console.log(sessions);
 
-        if (n >= 10 && n < 20) {
-            const newSession = {
-                time: n += 2,
-                timeId: Math.random().toString().substring(2, 4),
-                filmName: films[0]
-            }
-            films.shift();
-            setSession([...sessions, newSession]);
-            console.log(sessions.length);
-        }
-
-        sessions.map(session => {
-            return (
-                <Session session={session} key={session.timeId} />
-            )
-        })
     }
-
 
     return (
         <div className="calendar">
@@ -50,7 +27,13 @@ const Calendar = () => {
             <div className="date"><div className='content'>3 июня пт</div></div>
             <div className="date"><div className='content'>4 июня сб</div></div>
             <div className="date"><div className='content'>5 июня вс</div></div>
+         {   sessions.map((session) => {
+            return (
+                <Session session={session}  />
+            )
+        })}
         </div>
+        
     )
 }
 
