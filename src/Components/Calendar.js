e5c4e6ec8756ec6f602df9bb9ecbcf8bcbc3b796
anastasia <legkova.anastasia127@gmail.com>
2022-06-16 12:00:08 +0300

    sessions

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -5,7 +5,6 @@ import Session from './Session'
 
 const Calendar = () => {
     const [isActive, setActive] = useState(false);
-    // const [isActive, setActive] = useState()
 
     // useEffect(() => {
     //     setArray(array);
@@ -14,9 +13,9 @@ const Calendar = () => {
 
     // setSession(false)
     const sessionRender = () => {
-        
+
         setActive(!isActive)
-        
+
     }
 
 
@@ -26,14 +25,20 @@ const Calendar = () => {
                 <div className='content'>
                     1 июня ср
                 </div>
-                {/* <div> {isActive ? <Session /> : null} </div> */}
-<Session/>
+                <div> {isActive ? <Session /> : null} </div>
+                {/* <Session/> */}
             </div>
-            <div className="date"><div className='content'>2 июня чт</div></div>
-            <div className="date"><div className='content'>3 июня пт</div></div>
-            <div className="date"><div className='content'>4 июня сб</div></div>
-            <div className="date"><div className='content'>5 июня вс</div></div>
-            <div className='array'>
+            <div className="date"><div className='content'>2 июня чт</div>
+                <div> {isActive ? <Session /> : null} </div>
+            </div>
+            <div className="date"><div className='content'>3 июня пт</div>
+                <div> {isActive ? <Session /> : null} </div>
+            </div>
+            <div className="date"><div className='content'>4 июня сб</div>
+                <div> {isActive ? <Session /> : null} </div>
+            </div>
+            <div className="date"><div className='content'>5 июня вс</div>
+                <div> {isActive ? <Session /> : null} </div>
             </div>
         </div>
 
